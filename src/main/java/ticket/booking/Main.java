35ab7f5cd706d3a170a94c96877164c12286d786
kavya.dua <kavya.dua@shl.com>
2025-05-08 21:11:56 +0530

    feat: Added signup, login and fetchbookings functionality.

--- src/main/java/ticket/booking/Main.java
+++ src/main/java/ticket/booking/Main.java
@@ -1,17 +1,75 @@
 package ticket.booking;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import ticket.booking.services.UserBookingService;
+import ticket.booking.entities.Train;
+import ticket.booking.entities.User;
+import ticket.booking.utils.UserServiceUtil;
+
+import java.io.IOException;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Running Train Booking System");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int option = 0;
+
+        UserBookingService userBookingService;
+
+        try {
+            userBookingService = new UserBookingService();
+        }catch(IOException ex) {
+            System.out.print(ex);
+            System.out.println("There is something wrong");
+            return;
+        }
+
+        while(option!=7) {
+            System.out.println("Choose option");
+            System.out.println("1. Sign up");
+            System.out.println("2. Login");
+            System.out.println("3. Fetch Bookings");
+            System.out.println("4. Search Trains");
+            System.out.println("5. Book a Seat");
+            System.out.println("6. Cancel my Booking");
+            System.out.println("7. Exit the App");
+            option = scanner.nextInt();
+            switch (option){
+                case 1:
+                    System.out.println("Enter the username to signup");
+                    String nameToSignUp = scanner.next();
+                    System.out.println("Enter the password to signup");
+                    String passwordToSignUp = scanner.next();
+                    User userToSignup = new User(nameToSignUp, passwordToSignUp, UserServiceUtil.hashPassword(passwordToSignUp), new ArrayList<>(), UUID.randomUUID().toString());
+                    userBookingService.signUp(userToSignup);
+                    break;
+                case 2:
+                    System.out.println("Enter the username to Login");
+                    String nameToLogin = scanner.next();
+                    System.out.println("Enter the password to signup");
+                    String passwordToLogin = scanner.next();
+                    User userToLogin = new User(nameToLogin, passwordToLogin, UserServiceUtil.hashPassword(passwordToLogin), new ArrayList<>(), UUID.randomUUID().toString());
+                    try{
+                        userBookingService = new UserBookingService(userToLogin);
+                        if (userBookingService.loginUser()) {
+                            System.out.println("Login successful!");
+                        } else {
+                            System.out.println("Invalid username or password. Please try again.");
+                        }
+                    }catch (IOException ex){
+                        System.out.print(ex);
+                        return;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Fetching your bookings");
+                    userBookingService.fetchBookings();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
--- src/main/java/ticket/booking/services/UserBookingService.java
+++ src/main/java/ticket/booking/services/UserBookingService.java
@@ -2,25 +2,64 @@ package ticket.booking.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import ticket.booking.entities.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import ticket.booking.utils.UserServiceUtil;
 
 import java.io.IOException;
 import java.util.List;
 import java.io.File;
+import java.util.Optional;
 
 public class UserBookingService {
     private User user;
     private List<User> userList;
-    private static final String USERS_PATH = "../localDb/users.json";
+    private static final String USERS_PATH = "src\\main\\java\\ticket\\booking\\localDb\\users.json";
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public UserBookingService(User user) throws IOException {
-        this.user = user;
-        File users = new File(USERS_PATH);
-        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
-        });
+    public UserBookingService(User user1) throws IOException {
+        this.user = user1;
+        loadUserListFromFile();
     }
 
+    public UserBookingService() throws IOException {
+        loadUserListFromFile();
+    }
 
+    private void loadUserListFromFile() throws IOException {
+        userList = objectMapper.readValue(new File(USERS_PATH), new TypeReference<List<User>>() {});
+    }
 
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
 
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException {
+        File usersFile = new File(USERS_PATH);
+        objectMapper.writeValue(usersFile, userList);
+    }
+
+    public void fetchBookings() {
+        Optional<User> userFetched = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        if(userFetched.isPresent()){
+            userFetched.get().printTickets();
+        }
+    }
+
+    public Boolean cancelBookings(String ticketId) {
+        return Boolean.FALSE;
+    }
 }
